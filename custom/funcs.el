7442ac79f7c2dc3e0a879e994a9c805d8975905d
ibabanov <ilya.babanov@gmail.com>
2018-11-30 01:46:50 +0100

    First commit

---
+++ custom/funcs.el
@@ -0,0 +1,252 @@
+(defun p-eshell-prompt-function ()
+  (concat
+   (propertize (eshell/dirs) 'face `(:foreground "cornflower blue"))
+   (propertize "$ " 'face `(:foreground "green"))))
+
+(defun p-enable-evil-normal-state ()
+  (unless (member major-mode evil-normal-state-modes)
+    (message "Dynamically set evil state to 'normal for %s" (buffer-name))
+    (evil-set-initial-state major-mode 'normal)))
+
+(defun p-enable-evil-emacs-state ()
+  (unless (member major-mode evil-emacs-state-modes)
+    (message "Dynamically set evil state to 'emacs for %s" (buffer-name))
+    (evil-set-initial-state major-mode 'emacs)))
+
+(defun p-enable-scroll-margin ()
+  (make-variable-buffer-local 'scroll-margin)
+  (setq scroll-margin 10))
+
+(defun p-disable-scroll-margin ()
+  (make-variable-buffer-local 'scroll-margin)
+  (setq scroll-margin 0))
+
+(defun p-delete-tern-process ()
+  "Deletes tern process (after that, tern restarts automatically)"
+  (interactive)
+  (delete-process "Tern"))
+
+(defun p-term (name)
+  "Open term buffer with given name"
+  (interactive "sName: ")
+  (shell name)
+  (display-buffer name))
+
+(defun p-term-in-project ()
+  "Open term buffer in current project"
+  (interactive)
+  (let* ((default-directory (or (projectile-project-root) default-directory))
+         (buf-name (concat "*term:" (abbreviate-file-name default-directory) "*")))
+    (shell buf-name)
+    (display-buffer buf-name)))
+
+(defun p-npm-build ()
+  "Runs 'npm run build' command"
+  (interactive)
+  (let ((bpr-scroll-direction -1))
+    (bpr-spawn "npm run build")))
+
+(defvar p-boo-role "app"
+  "Default role for p-boo* commands")
+
+(defun p-boo-set-role (role)
+  "Sets default role for p-boo* commands"
+  (interactive "sRole: ")
+  (setq p-boo-role role))
+
+(defun p-boo-run (command project)
+  "Runs 'boo [command] [project]"
+  (let ((bpr-scroll-direction -1))
+    (bpr-spawn (concat "boo " command " " project))))
+
+(defun p-boo-restart ()
+  "Runs 'boo restart ROLE'"
+  (interactive)
+  (p-boo-run "restart" p/boo-role))
+
+(defun p-boo-sync ()
+  "Runs 'boo sync ROLE'"
+  (interactive)
+  (p-boo-run "sync" p/boo-role))
+
+(defun p-boo-sync-restart ()
+  "Runs 'boo sync ROLE && boo restart ROLE'"
+  (interactive)
+  (bpr-spawn (concat "boo sync " p-boo-role " && boo restart " p/boo-role)))
+
+(defun p-boo-concat ()
+  "Runs 'boo concat ROLE'"
+  (interactive)
+  (bpr-spawn (concat "boo sync " p-boo-role " && boo concat " p/boo-role)))
+
+(defun p-bpr-package-tests ()
+  "Tests emacs-bpr package"
+  (interactive)
+  (let* ((bpr-process-directory "~/projects/emacs-bpr/")
+         (bpr-scroll-direction -1))
+    (bpr-spawn "cask exec buttercup -L .")))
+
+(defun p-restart-wifi-osx ()
+  "Restarts wifi on osx"
+  (interactive)
+  (let* ((bpr-process-directory "~/"))
+    (bpr-spawn "networksetup -setairportpower en0 off; sleep 4; networksetup -setairportpower en0 on")))
+
+(defun p-flyspell-save-word ()
+  "Saves word to flyspell current dictionary"
+  (interactive)
+  (let ((current-location (point))
+        (word (flyspell-get-word)))
+    (when (consp word)
+      (message "Saving word '%s' to dictionary" (car word))
+      (flyspell-do-correct 'save nil
+                           (car word)
+                           current-location
+                           (cadr word)
+                           (caddr word)
+                           current-location))))
+
+(defun p-eval-line (eval-region)
+  "Evaluates current line"
+  (save-mark-and-excursion
+    (end-of-line)
+    (set-mark (line-beginning-position))
+    (call-interactively eval-region)))
+
+(defun p-eval-region-or-line (eval-region)
+  "Evaluates active region or current line"
+  (if (use-region-p)
+      (call-interactively eval-region)
+    (p-eval-line eval-region)))
+
+(defun p-eval-py ()
+  "Evaluatetes active region or current line as python code"
+  (interactive)
+  (p-eval-region-or-line 'python-shell-send-region))
+
+(defun p-copy-to-clipboard (text)
+  "Copies text to clipboard"
+  (when text
+    (kill-new text)
+    (message "Text is copied to clipboard: %s" text)))
+
+(defun p-get-file-name ()
+  "Returns file path of the current buffer"
+  (if (equal major-mode 'dired-mode)
+      default-directory
+    (buffer-file-name)))
+
+(defun p-copy-file-name ()
+  "Copies file name to clipboard"
+  (interactive)
+  (p-copy-to-clipboard (p/get-file-name)))
+
+(defun p-copy-file-name-and-line-number ()
+  "Copies file name with current line number to clipboard"
+  (interactive)
+  (p-copy-to-clipboard
+   (concat (p-get-file-name) ":" (number-to-string (line-number-at-pos)))))
+
+(defun p-magit-show-mine-commits ()
+  "Logs mine commits"
+  (interactive)
+  (magit-log nil '("-n128" "--decorate" "--author=babanov")))
+
+(defun p-shell-show-mine-commits ()
+  "Logs mine commits"
+  (interactive)
+  (async-shell-command
+   "git log --author='Ilya Babanov' --pretty=format:'%h - %an, %ar : %s' -n 10"))
+
+(defun p-git-push ()
+  "git pull && git push"
+  (interactive)
+  (bpr-spawn "git pull && git push"))
+
+(defun p-git-pull ()
+  "git pull"
+  (interactive)
+  (bpr-spawn "git pull"))
+
+(defun p-insert-g-arg ()
+  "Inserts ' -g*.' text"
+  (interactive)
+  (insert-for-yank " -g*"))
+
+(defun p-insert-g-js-arg ()
+  "Inserts ' -g*.js' text"
+  (interactive)
+  (insert-for-yank " -g*.js"))
+
+(defun p-bpr-set-comint-filter (process)
+  "Sets 'comint-output-filter to the process"
+  (set-process-filter process 'comint-output-filter))
+
+(defun p-evil-search-clear-highlight ()
+  "Clear evil-search or evil-ex-search persistent highlights."
+  (interactive)
+  ;; (evil-search-highlight-persist-remove-all) ; `C-s' highlights
+  (evil-ex-nohighlight))                     ; `/' highlights
+
+(defun p-show-trailing-whitespace ()
+  "Sets show-trailing-whitespace to t"
+  (interactive)
+  (setq show-trailing-whitespace t))
+
+(defun p-toggle-show-trailing-whitespace ()
+  "Toggles show-trailing-whitespace between t and nil"
+  (interactive)
+  (setq show-trailing-whitespace (not show-trailing-whitespace)))
+
+(defun p-lisp-insert-sexp-after ()
+  "Insert sexp after the current one."
+  (interactive)
+  (let ((sp-navigate-consider-symbols nil))
+    (if (char-equal (char-after) ?\() (forward-char))
+    (sp-up-sexp)
+    (evil-insert-state)
+    (sp-newline)
+    (sp-insert-pair "(")))
+
+(defun p-lisp-insert-sexp-before ()
+  "Insert sexp before the current one."
+  (interactive)
+  (let ((sp-navigate-consider-symbols nil))
+    (if (char-equal (char-after) ?\() (forward-char))
+    (sp-backward-sexp)
+    (evil-insert-state)
+    (sp-newline)
+    (evil-previous-visual-line)
+    (evil-end-of-line)
+    (insert " ")
+    (sp-insert-pair "(")
+    (indent-for-tab-command)))
+
+(defun p-create-newline-and-enter-sexp (&rest _ignored)
+  "Open a new brace or bracket expression, with relevant newlines and indent. "
+  (newline)
+  (indent-according-to-mode)
+  (forward-line -1)
+  (indent-according-to-mode))
+
+(defun p-send-to-first-visible-shell ()
+  (interactive)
+  (if-let* ((comint-window (get-window-with-predicate 'p-is-comint-window)))
+      (p-send-to-shell (buffer-name (window-buffer comint-window)))
+    (message "No visible comint buffers")))
+
+(defun p-is-comint-window (window)
+  (with-current-buffer (window-buffer window)
+    (derived-mode-p 'comint-mode)))
+
+(defun p-send-to-shell (shell)
+  (let ((string (p-get-string-based-on-context)))
+    (process-send-string shell (p-format-string-for-process string))))
+
+(defun p-get-string-based-on-context ()
+  (if (use-region-p)
+      (buffer-substring (region-beginning) (region-end))
+    (thing-at-point 'line)))
+
+(defun p-format-string-for-process (string)
+  (concat string "\n"))
---
+++ init.el
@@ -0,0 +1,416 @@
+;; init.el --- description -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Hello there!
+;;; Code:
+
+(require 'package)
+(setq package-enable-at-startup nil)
+(setq package-archives '(("org"   . "http://orgmode.org/elpa/")
+                         ("gnu"   . "http://elpa.gnu.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")))
+
+(unless package--initialized (package-initialize t))
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+(load "~/.emacs.d/custom/funcs.el")
+
+(add-to-list 'default-frame-alist '(font . "Hack-12"))
+(add-to-list 'default-frame-alist '(height . 40))
+(add-to-list 'default-frame-alist '(width . 80))
+
+(when window-system
+  (scroll-bar-mode 0)
+  (tool-bar-mode 0)
+  (tooltip-mode 0)
+  (menu-bar-mode 0))
+
+(setq-default
+ ad-redefinition-action 'accept
+ auto-window-vscroll nil
+ cursor-in-non-selected-windows t
+ delete-by-moving-to-trash t
+ display-time-default-load-average nil
+ display-time-format "%H:%M"
+ fill-column 80
+ help-window-select t
+ indent-tabs-mode nil
+ inhibit-startup-screen t
+ initial-scratch-message ""
+ mouse-yank-at-point t
+ recenter-positions '(5 top bottom)
+ scroll-conservatively most-positive-fixnum
+ scroll-margin 10
+ select-enable-clipboard t
+ sentence-end-double-space nil
+ show-trailing-whitespace nil
+ split-height-threshold nil
+ split-width-threshold nil
+ tab-width 4
+ uniquify-buffer-name-style 'forward
+ window-combination-resize t
+ x-stretch-cursor t)
+
+;; (setq-default left-margin-width 1 right-margin-width 1)
+;; (fringe-mode 0)
+;; (global-subword-mode 1)
+
+(global-auto-revert-mode 1)
+(show-paren-mode 1)
+(delete-selection-mode 1)
+(fset 'yes-or-no-p 'y-or-n-p)
+(mouse-avoidance-mode 'banish)
+(put 'downcase-region 'disabled nil)
+(put 'upcase-region 'disabled nil)
+(set-default-coding-systems 'utf-8)
+
+(add-hook 'prog-mode-hook (lambda () (setq show-trailing-whitespace t)))
+
+(use-package exec-path-from-shell
+  :config
+  (setq exec-path-from-shell-arguments '("-l" "-i"))
+  (when (memq window-system '(mac ns x))
+    (exec-path-from-shell-initialize)))
+
+(use-package all-the-icons)
+
+(use-package doom-themes
+  :config
+  (load-theme 'doom-solarized-light t)
+  (doom-themes-neotree-config))
+
+(use-package solaire-mode
+  :hook ((change-major-mode after-revert ediff-prepare-buffer) . turn-on-solaire-mode)
+  :config
+  (add-hook 'minibuffer-setup-hook #'solaire-mode-in-minibuffer))
+
+(use-package spaceline
+  :init
+  (setq powerline-default-separator nil)
+  :config
+  (spaceline-emacs-theme)
+  (spaceline-toggle-minor-modes-off)
+  (spaceline-toggle-buffer-size-off)
+  (spaceline-toggle-evil-state-on))
+
+(use-package dashboard
+  :config
+  (setq dashboard-items '((recents  . 10)
+                          (projects . 5)
+                          (bookmarks . 5)))
+  (dashboard-setup-startup-hook))
+
+(use-package evil
+  :init
+  (setq evil-want-C-u-scroll t)
+  :config
+  (evil-mode 1))
+
+(use-package anzu
+  :config
+  (global-anzu-mode 1)
+  (global-set-key [remap query-replace-regexp] 'anzu-query-replace-regexp)
+  (global-set-key [remap query-replace] 'anzu-query-replace))
+
+(use-package helm
+  :init
+  (setq-default
+   helm-default-display-buffer-functions '(display-buffer-in-side-window)
+   helm-always-two-windows nil
+   helm-M-x-fuzzy-match t
+   helm-mode-fuzzy-match t
+   helm-buffers-fuzzy-matching t
+   helm-recentf-fuzzy-match t
+   helm-locate-fuzzy-match t
+   helm-semantic-fuzzy-match t
+   helm-imenu-fuzzy-match t
+   helm-completion-in-region-fuzzy-match t
+   helm-candidate-number-list 80
+   helm-split-window-in-side-p t
+   helm-move-to-line-cycle-in-source nil
+   helm-echo-input-in-header-line t
+   helm-autoresize-max-height 0
+   helm-autoresize-min-height 20)
+  :config
+  (helm-mode 1))
+
+(use-package helm-ag
+  :config
+  (setq helm-ag-base-command "rg --smart-case --no-heading --line-number --max-columns 150"))
+
+(use-package projectile
+  :init
+  (setq projectile-require-project-root nil)
+  :config
+  (projectile-mode 1))
+
+(use-package helm-projectile
+  :init
+  (setq helm-projectile-fuzzy-match t)
+  :config
+  (helm-projectile-on))
+
+(use-package flycheck
+  :init (global-flycheck-mode))
+
+(use-package neotree
+  :init
+  (setq neo-theme (if (display-graphic-p) 'icons 'arrow)))
+
+(use-package evil-nerd-commenter
+  :commands (evilnc-comment-or-uncomment-lines))
+
+(use-package buttercup)
+
+(use-package bpr
+  :commands (bpr-spawn bpr-open-last-buffer)
+  :config
+  (setq bpr-colorize-output t)
+  (setq bpr-on-start 'p-bpr-set-comint-filter))
+
+(use-package magit)
+
+(use-package git-gutter
+  :commands git-gutter-mode
+  :init
+  (add-hook 'prog-mode-hook #'git-gutter-mode))
+
+(use-package shackle
+  :config
+  (setq shackle-rules
+        '(("\\*helm.*?\\*" :regexp t :align t :size 0.5)
+          ("\\*term:.*?\\*" :regexp t :align t :select t :size 0.6)
+          ("\\*.*\\*" :regexp t :align t :select t :size 0.5)))
+  (shackle-mode))
+
+(use-package emmet-mode
+  :hook (css-mode html-mode rjsx-mode)
+  :config
+  (setq-default
+   emmet-insert-flash-time .1
+   emmet-move-cursor-between-quote t))
+
+(use-package yasnippet
+  :config
+  (add-hook 'prog-mode-hook 'yas-minor-mode)
+  (setq-default yas-snippet-dirs `(,(expand-file-name "snippets/" user-emacs-directory)))
+  (yas-reload-all))
+
+(use-package editorconfig
+  :config
+  (editorconfig-mode))
+
+(use-package rainbow-delimiters
+  :config
+  (add-hook 'prog-mode-hook #'rainbow-delimiters-mode))
+
+(use-package electric
+  :config
+  (add-hook 'prog-mode-hook 'electric-indent-mode))
+
+(use-package elec-pair
+  :config
+  (add-hook 'prog-mode-hook 'electric-pair-mode))
+
+(use-package files
+  :ensure nil
+  :config
+  (setq-default
+   backup-by-copying t
+   backup-directory-alist `(("." . ,(expand-file-name "backups/" user-emacs-directory)))
+   delete-old-versions t
+   version-control t))
+
+(use-package lsp-mode
+  :init
+  (add-hook 'prog-major-mode #'lsp-prog-major-mode-enable))
+
+;;(use-package lsp-ui
+;;  :init
+;;  (add-hook 'lsp-mode-hook 'lsp-ui-mode))
+
+(use-package company
+  :init
+  (setq company-minimum-prefix-length 3)
+  (setq company-idle-delay 0)
+  (setq company-require-match 'never)
+  (setq tab-always-indent 'complete)
+  :config
+  (global-company-mode 1)
+  (define-key company-active-map (kbd "TAB") 'company-complete-common-or-cycle)
+  (define-key company-active-map (kbd "<tab>") 'company-complete-common-or-cycle)
+  (define-key company-active-map (kbd "S-TAB") 'company-select-previous)
+  (define-key company-active-map (kbd "<backtab>") 'company-select-previous))
+
+(use-package company-quickhelp
+  :config
+  (setq company-quickhelp-delay nil)
+  (company-quickhelp-mode))
+
+(use-package company-lsp
+  :commands (company-lsp)
+  :init
+  (setq company-lsp-async t)
+  (push #'company-lsp company-backends))
+
+(use-package js2-mode
+  :demand t
+  :mode "\\.js\\'")
+
+(use-package typescript-mode
+  :demand t
+  :mode "\\.ts\\'")
+
+;; (use-package tern :ensure t)
+
+(use-package rust-mode
+  :demand t
+  :mode "\\.rs\\'")
+
+(use-package lsp-rust
+  :init
+  (setq lsp-rust-rls-command '("rustup" "run" "nightly" "rls"))
+  (add-hook 'rust-mode-hook #'lsp-rust-enable))
+
+(use-package lsp-javascript-typescript
+  :init
+  (add-to-list 'js-mode-hook #'lsp-javascript-typescript-enable)
+  (add-to-list 'typescript-mode-hook #'lsp-javascript-typescript-enable))
+
+(use-package js2-mode
+  :config
+  (setq-default
+   js2-idle-timer-delay 0
+   js2-strict-trailing-comma-warning nil
+   js2-strict-missing-semi-warning nil
+   js2-mode-show-parse-errors nil
+   js2-mode-show-strict-warnings nil))
+
+(use-package rjsx-mode
+  :mode "\\.jsx\\'")
+
+(use-package csv-mode
+  :config
+  (setq-default csv-align-padding 2))
+
+(use-package dockerfile-mode
+  :mode "Dockerfile\\'")
+
+(use-package yaml-mode
+  :mode "\\.ya?ml\\'")
+
+(use-package web-mode
+  :mode (("\\.erb\\'" . web-mode)
+         ("\\.mustache\\'" . web-mode)
+         ("\\.html?\\'" . web-mode)
+         ("\\.php\\'" . web-mode)))
+
+(use-package go-mode
+  :mode "\\.go\\'")
+
+(use-package which-key
+  :init
+  (setq which-key-separator " ")
+  (setq which-key-prefix-prefix "+")
+  :config
+  (which-key-mode))
+
+(use-package general
+  :config
+  (general-define-key
+   "<escape>" '(keyboard-escape-quit :which-key "escape")
+   "<f4>" '(p-term-in-project :which-key "terminal in project"))
+
+  (general-define-key
+   :states '(normal visual insert emacs)
+   :prefix "SPC"
+   :non-normal-prefix "M-SPC"
+
+   "SPC" '(helm-M-x :which-key "M-x")
+   "TAB" '(switch-to-prev-buffer :which-key "previous buffer")
+   "RET" '(helm-bookmarks :which-key "bookmark")
+   "/"   '(helm-do-ag-project-root :which-key "search in project")
+   "a"   '(helm-mini :which-key "buffers list")
+   ";"   '(evilnc-comment-or-uncomment-lines :which-key "comment")
+   "."   '(helm-find-files :which-key "find files")
+
+   "bk"  '(kill-this-buffer :which-key "kill this buffer")
+
+   "cd"  '(lsp-goto-type-definition :which-key "go to definition")
+   "ci"  '(lsp-goto-implementation :which-key "go to implementation")
+   "ce"  '(flycheck-list-errors :which-key "list errors")
+
+   "ff"  '(helm-projectile-find-file :which-key "find files")
+   "fp"  '(helm-projectile-find-file :which-key "find files in project")
+   "fy"  '(p-copy-file-name-and-line-number :which-key "copy file name and line number")
+
+   "gg"  '(magit :which-key "magit")
+   "gb"  '(magit-blame :which-key "git blame")
+   "gl"  '(magit-log-buffer-file :which-key "log current file")
+   "gm"  '(p-shell-show-mine-commits :which-key "show mine commits")
+
+   "ot"  '(p-term-in-project :which-key "open terminal")
+   "on"  '(neotree-toggle :which-key "toggle neotree")
+
+   "pf"  '(helm-projectile-find-file :which-key "find files in project")
+   "pp"  '(helm-projectile-switch-project :which-key "switch project")
+   "pb"  '(helm-projectile-switch-to-buffer :which-key "switch buffer")
+
+   "tn"  '(neotree-toggle :which-key "toggle neotree")
+
+   "qz"  '(delete-frame :which-key "delete frame")
+   "qq"  '(kill-emacs :which-key "quit")
+
+   "hh"  '(helm-apropos :which-key "apropos")
+   "jk"  '(helm-show-kill-ring :which-key "show kill ring")
+   "jr"  '(helm-resume :which-key "helm-resume")
+
+   "wl"  '(windmove-right :which-key "move right")
+   "wh"  '(windmove-left :which-key "move left")
+   "wk"  '(windmove-up :which-key "move up")
+   "wj"  '(windmove-down :which-key "move bottom")
+   "wv"  '(split-window-right :which-key "split right")
+   "ws"  '(split-window-below :which-key "split bottom")
+   "wc"  '(delete-window :which-key "delete window"))
+
+  (general-define-key
+   :keymaps 'helm-ag-map
+   "C-t" '(p-insert-g-arg :which-key "insert rg argument"))
+
+  (general-define-key
+   :keymaps 'neotree-mode-map
+   :states 'normal
+   "TAB" 'neotree-quick-look
+   "RET" 'neotree-enter
+   "c"   'neotree-create-node
+   "r"   'neotree-rename-node
+   "d"   'neotree-delete-node
+   "j"   'neotree-next-line
+   "k"   'neotree-previous-line
+   "n"   'neotree-next-line
+   "p"   'neotree-previous-line
+   "J"   'neotree-select-next-sibling-node
+   "K"   'neotree-select-previous-sibling-node
+   "H"   'neotree-select-up-node
+   "L"   'neotree-select-down-node
+   "v"   'neotree-enter-vertical-split
+   "s"   'neotree-enter-horizontal-split
+   "q"   'neotree-hide
+   "R"   'neotree-refresh)
+
+  (general-define-key
+   :keymaps 'company-active-map
+   "C-i" '(company-quickhelp-manual-begin :which-key "show docs")))
+
+(setq-default custom-file (expand-file-name ".custom.el" user-emacs-directory))
+(when (file-exists-p custom-file)
+  (load custom-file))
+
+(provide 'init)
+
+;;; init.el ends here

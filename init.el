eec85fcbd392e5f629ee15616b2fa2b0917177ee
ibabanov <ilya.babanov@gmail.com>
2018-12-02 14:28:43 +0100

    Enable web-mode for .vue files

--- init.el
+++ init.el
@@ -317,6 +317,7 @@
 (use-package web-mode
   :mode (("\\.erb\\'" . web-mode)
          ("\\.mustache\\'" . web-mode)
+         ("\\.vue\\'" . web-mode)
          ("\\.html?\\'" . web-mode)
          ("\\.php\\'" . web-mode)))
 
